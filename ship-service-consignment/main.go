dfa807a67f8c2540441e20322c3d54c8d046d885
Kumar Ramanathan <5899848+kramanathan01@users.noreply.github.com>
2020-07-28 13:42:09 -0700

    Initial commit
    Signed-off-by: Kumar Ramanathan <5899848+kramanathan01@users.noreply.github.com>

---
+++ ship-service-consignment/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+	"sync"
+
+	pb "kr.com/ship-consignment-service/proto/consignment/consignment.pb.go"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+const (
+	port = ":50501"
+)
+
+type repository interface {
+	Create(*pb.Consignment) (*pb.Consignment, error)
+}
+
+// Repository - Dummy Repo for now
+type Repository struct {
+	mu           sync.RWMutex
+	consignments []*pb.Consignment
+}
+
+// Create a new Consignment
+func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
+	repo.mu.Lock()
+	u := append(repo.consignments, consignment)
+	repo.consignments = u
+	repo.mu.Unlock()
+
+	return consignment, nil
+}
+
+// Service implements the gRPC interface
+type service struct {
+	repo repository
+}
+
+// CreateConsignment - Create method that takes in a protobuf Consignment and
+// returns protobuf Response message
+
+func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment) (*pb.Response, error) {
+
+	consignment, err := s.repo.Create(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.Response{Created: true, Consignment: consignment}, nil
+}
+
+func main() {
+
+	repo := &Repository{}
+
+	// Set up gRPC server
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen %v", err)
+	}
+	s := grpc.NewServer()
+
+	// Register our service as handler for protobuf interface
+	pb.RegisterShippingServiceServer(s, &service{repo})
+	reflection.Register(s)
+
+	log.Println("Running on port:", port)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
